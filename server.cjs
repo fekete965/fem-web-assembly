f8cbd4389ee350f773052c0465ab64f451b18c34
Bence Fekete <fekete965@gmail.com>
2024-06-26 21:58:39 +0100

    Improve up our server

--- server.cjs
+++ server.cjs
@@ -4,9 +4,13 @@ const PORT = 3000;
 
 const app = express();
 
-// app.use(express.static("./public"));
-app.use(express.static("./"));
-// app.use(express.static("./js"));
+app.use(express.static("./public"));
+app.use("/js", express.static("./js"));
+app.use("/build", express.static("./build"));
+
+app.get("/", (_, res) => {
+  res.sendFile("index.html");
+});
 
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
